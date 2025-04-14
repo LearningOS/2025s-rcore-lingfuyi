1b152a7baf3bd282c91d91d137f61e017c5a112f
lingfuyi <lingfuyi@gmail.com>
2025-04-14 17:22:01 +0800

    完成ch3作业

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,6 +1,6 @@
 //! Process management syscalls
 use crate::{
-    task::{exit_current_and_run_next, suspend_current_and_run_next},
+    task::{exit_current_and_run_next, get_sys_call_times, suspend_current_and_run_next},
     timer::get_time_us,
 };
 
@@ -37,9 +37,35 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     }
     0
 }
+/*
+这个系统调用有三种功能，根据 trace_request 的值不同，执行不同的操作：
+如果 trace_request 为 0，则 id 应被视作 *const u8 ，表示读取当前任务 id 地址处一个字节的无符号整数值。此时应忽略 data 参数。返回值为 id 地址处的值。
+如果 trace_request 为 1，则 id 应被视作 *const u8 ，表示写入 data （作为 u8，即只考虑最低位的一个字节）到该用户程序 id 地址处。返回值应为0。
+如果 trace_request 为 2，表示查询当前任务调用编号为 id 的系统调用的次数，返回值为这个调用次数。本次调用也计入统计 。
+否则，忽略其他参数，返回值为 -1。
+ */
 
-// TODO: implement the syscall
-pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
+pub fn sys_trace(trace_request: usize, id: usize, data: usize) -> isize {
     trace!("kernel: sys_trace");
-    -1
+    match trace_request {
+        0 => {
+            // 读取用户程序 id 地址处的一个字节
+            let ptr = id as *const u8;
+            unsafe { *ptr as isize }
+        }
+        1 => {
+            // 写入 data 的最低字节到用户程序 id 地址处
+            let ptr = id as *mut u8;
+            unsafe {
+                *ptr = data as u8;
+            }
+            0
+        }
+        2 => {
+            // 返回当前任务调用编号为 id 的系统调用的次数
+            let times = get_sys_call_times();
+            times[id] as isize
+        }
+        _ => -1,
+    }
 }
--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -1,6 +1,6 @@
 //! Types related to task management
-
 use super::TaskContext;
+use crate::config::MAX_SYSCALL_NUM;
 
 /// The task control block (TCB) of a task.
 #[derive(Copy, Clone)]
@@ -9,6 +9,8 @@ pub struct TaskControlBlock {
     pub task_status: TaskStatus,
     /// The task context
     pub task_cx: TaskContext,
+    /// The syscall times of the task
+    pub sys_call_times: [u32; MAX_SYSCALL_NUM],
 }
 
 /// The status of a task
